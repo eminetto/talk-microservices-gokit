96a9d043c5256ced81b2d292196383e2dbeea572
Elton Minetto <elton.minetto@betrybe.com>
2021-02-01 14:48:18 -0300

    feat: add middleware

--- votes/vote/transport_http.go
+++ votes/vote/transport_http.go
@@ -3,6 +3,7 @@ package vote
 import (
 	"net/http"
 
+	"github.com/eminetto/talk-microservices-gokit/pkg/middleware"
 	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/gorilla/mux"
@@ -19,6 +20,7 @@ func NewHttpServer(svc Service, logger log.Logger) {
 		options...,
 	)
 	r := mux.NewRouter()
+	r.Use(middleware.IsAuthenticatedMiddleware)
 	r.Methods("POST").Path("/v1/vote").Handler(storeHandler)
 	logger.Log("msg", "HTTP", "addr", "8083")
 	logger.Log("err", http.ListenAndServe(":8083", r))
